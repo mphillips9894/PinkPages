a7fa324ee8f0dce82040faef69afb8143a2b18b7
Casey Madden <casey.c.madden@gmail.com>
2017-05-16 10:45:29 -0400

    accordian first try

--- public/js/controllers/MapsCtrl.js
+++ public/js/controllers/MapsCtrl.js
@@ -65,30 +65,13 @@ angular.module('MapsCtrl', [])
             else Resource.get($scope.cat).then(callback, cberr);
 
         });
-
-        $scope.oneAtATime = true;
-
-      $scope.groups = [
-        {
-          title: 'Dynamic Group Header - 1',
-          content: 'Dynamic Group Body - 1'
-        },
-        {
-          title: 'Dynamic Group Header - 2',
-          content: 'Dynamic Group Body - 2'
-        }
-      ];
-
-      $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-      $scope.addItem = function() {
-        var newItemNo = $scope.items.length + 1;
-        $scope.items.push('Item ' + newItemNo);
-      };
-
-      $scope.status = {
-        isCustomHeaderOpen: false,
-        isFirstOpen: true,
-        isFirstDisabled: false
-      };
-    }]);
+    }])
+    .filter('catFilter', function(){
+        return function(items, cat){
+            var completed = [];
+            angular.forEach(items, function(item){
+                if(item.category === cat) completed.push(item);
+            });
+            return completed;
+        };
+    });
